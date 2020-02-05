8600b917ba610616b71351f7938b50438607ed47
tawatchaiaem <tawatchai.h1994@gmail.com>
2020-02-05 17:37:25 +0700

    all

--- 1-NODE-FARM/index.js
+++ 1-NODE-FARM/index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+////////////////////////////
+// FILES
 
 // Blocking, syschronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -9,15 +14,67 @@ const fs = require('fs');
 // console.log('File written');
 
 // Non-blocking, asyschronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`,'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile('./txt/append.txt','utf-8', (err,data3) => {
-            console.log(data3);
-            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err => {
-                console.log('Your file has been written');
-            });
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`,'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt','utf-8', (err,data3) => {
+//             console.log(data3);
+//             fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err => {
+//                 console.log('Your file has been written');
+//             });
+//         });
+//     });
+// });
+// console.log('Will read file!');
+
+////////////////////////////
+// SERVERS
+const replaceTemplate = (temp, product) =>{
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productaName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+const temOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req,res) => {
+    const pathName = req.url;
+    // Overview Page
+    if(pathName === '/' || pathName === '/overview'){
+        res.writeHead(200,{'Content-type':'text/html'});
+        const cardHtml = dataObj.map(el => replaceTemplate(tempCard, el));
+        console.log(cardHtml);
+        res.end(temOverview);
+
+    // Product Page
+    }else if(pathName === '/product'){
+        res.end('This is the PRODUCT');
+    // API
+    }else if(pathName === '/api'){
+        res.writeHead(200,{'Content-type' : 'application/json'})
+        res.end(data);
+    // Not found
+    }else{
+        res.writeHead(404,{
+            'Content-type' : 'text/html',
+            'my-own-header': 'hello word!'
         });
-    });
+        res.end('<h1>Page not found!</h1>');
+    }
 });
-console.log('Will read file!');
+
+server.listen(8000,'127.0.0.1', () => {
+    console.log('Listening to request on port 8000 ');
+});
