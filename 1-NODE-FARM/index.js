830ad15b2e92cf0a4fe11673994b79c5bb879410
tawatchai hona <programmer.test.th@gmail.com>
2020-02-06 00:05:57 +0700

    lesson 15

--- 1-NODE-FARM/index.js
+++ 1-NODE-FARM/index.js
@@ -30,7 +30,7 @@ const url = require('url');
 ////////////////////////////
 // SERVERS
 const replaceTemplate = (temp, product) =>{
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productaName);
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
     output = output.replace(/{%IMAGE%}/g, product.image);
     output = output.replace(/{%PRICE%}/g, product.price);
     output = output.replace(/{%FROM%}/g, product.from);
@@ -38,11 +38,18 @@ const replaceTemplate = (temp, product) =>{
     output = output.replace(/{%QUANTITY%}/g, product.quantity);
     output = output.replace(/{%DESCRIPTION%}/g, product.description);
     output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    
+    if(product.organic != true) 
+    {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+      
+    }else{
+       output = output.replace(/{%NOT_ORGANIC%}/g, ''); 
+    }
+    
     return output;
 }
-const temOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
 
@@ -54,9 +61,12 @@ const server = http.createServer((req,res) => {
     // Overview Page
     if(pathName === '/' || pathName === '/overview'){
         res.writeHead(200,{'Content-type':'text/html'});
-        const cardHtml = dataObj.map(el => replaceTemplate(tempCard, el));
-        console.log(cardHtml);
-        res.end(temOverview);
+
+        const cardHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardHtml);
+
+        res.end(output);
 
     // Product Page
     }else if(pathName === '/product'){
