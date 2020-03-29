bd791c961210b70c1ca340d61212f5befe2d09b3
tawatchai hona <programmer.test.th@gmail.com>
2020-03-29 22:57:40 +0700

    eslint pugin

--- 4-NATOURS/server.js
+++ 4-NATOURS/server.js
@@ -1,11 +1,9 @@
 const dotenv = require('dotenv');
-const app = require('./app');
-
 dotenv.config({path: './config.env'});
 
-console.log(process.env);
+const app = require('./app');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
