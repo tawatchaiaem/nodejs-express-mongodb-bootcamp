2a3e48f4a9274dea6038dd55c79c9199b5181267
tawatchai hona <programmer.test.th@gmail.com>
2020-04-09 23:00:50 +0700

    env

--- 4-NATOURS/app.js
+++ 4-NATOURS/app.js
@@ -7,7 +7,8 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) MIDDLEWARES
-console.log(process.env.NODE_ENV );
+
+//console.log(process.env.PORT);
 if(process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'));
 }
--- 4-NATOURS/server.js
+++ 4-NATOURS/server.js
@@ -1,6 +1,11 @@
-const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
 
+const dotenv = require('dotenv');
+//dotenv.config({path: '.env'});
+
+
+console.log(dotenv.config({path: './.env'}));
+console.log(process.env.NODE_ENV);
+console.log(process.env);
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
